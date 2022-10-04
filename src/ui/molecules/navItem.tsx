08ef4f8d6bec1d2efa4a5b58f3bfc4ea7e19daeb
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:15:22 +0800

    molecule-navitem-add

---
+++ src/ui/molecules/navItem.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import { Text } from "../atoms";
+import { COLORS } from "../../config/colors";
+
+interface INavItem {
+  children: string;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  active?: boolean;
+}
+
+const NavItem: React.FC<INavItem> = ({ children, onClick, active }) => {
+  const [isHover, setIshover] = useState(false);
+
+  return (
+    <div
+      className="molecule-navitem"
+      onMouseEnter={() => {
+        setIshover(true);
+      }}
+      onMouseLeave={() => {
+        setIshover(false);
+      }}
+      onClick={onClick}
+    >
+      <Text
+        color={
+          active || isHover
+            ? COLORS.TEXT_PRIMARY_DARK
+            : COLORS.TEXT_SECONDARY_DARK
+        }
+      >
+        {children}
+      </Text>
+    </div>
+  );
+};
+
+export default NavItem;
