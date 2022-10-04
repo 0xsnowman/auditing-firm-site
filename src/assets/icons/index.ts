af3c202dbcfa0afda77abc522f3da5cb706ae11a
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:12:39 +0800

    icon-add

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+import rocket from "./rocket.svg";
+import blockchain from "./blockchain.svg";
+import search from "./search.svg";
+
+const Icons = {
+    rocket,
+    blockchain,
+    search,
+};
+
+export default Icons;
