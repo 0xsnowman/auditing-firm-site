ff8950664c78fd3b2cc2106b2fa0b257ec36b7a1
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:16:51 +0800

    molecule-page-add

---
+++ src/ui/molecules/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// components
+import { Box } from "../atoms";
+
+import { COLORS } from "config/colors";
+
+interface IPageProps {
+  children:
+    | boolean
+    | JSX.Element
+    | (boolean | JSX.Element)[]
+    | (boolean | JSX.Element[])[];
+  className?: string;
+  paddingVertical?: number;
+}
+
+const Page: React.FC<IPageProps> = ({
+  children,
+  className,
+  paddingVertical = 120
+}) => {
+  return (
+    <Box
+      className={["molecule-page", className].join(" ")}
+      paddingVertical={paddingVertical}
+      backgroundColor={COLORS.GREY}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default Page;
