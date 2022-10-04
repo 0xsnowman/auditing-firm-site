0811a62497a7c1e52f4167e7dd8ce7e6d1d8a79f
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:14:12 +0800

    molecule-iconButton-add

---
+++ src/ui/molecules/iconButton.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+import { Text, Icon, Box, Flex } from "ui/atoms";
+import { PADDING, BORDER_RADIUS } from "config/dimensions";
+
+interface IIconButtonProps {
+  children: string;
+  className?: string;
+  icon: string;
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  isIconLeft?: boolean;
+  backgroundColor?: string;
+  fixedWidth?: boolean;
+  disabled?: boolean;
+}
+
+const IconButton: React.FC<IIconButtonProps> = ({
+  children,
+  className,
+  icon,
+  onClick,
+  isIconLeft = false,
+  backgroundColor = "#000000",
+  fixedWidth = false,
+  disabled = false
+}) => {
+  const [isHover, setIsHover] = React.useState(false);
+  return (
+    <div
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
+    >
+      <Box
+        className={[
+          "molecule-iconbutton",
+          className,
+          disabled && "molecule-iconbutton-disabled"
+        ].join(" ")}
+        backgroundColor={backgroundColor}
+        paddingHorizontal={PADDING.MEDIUM}
+        borderRadius={BORDER_RADIUS.SMALL}
+        onClick={disabled ? () => {} : onClick}
+        width={fixedWidth ? 168 : undefined}
+        boxShadow={
+          !disabled && isHover
+            ? `0px 0px 20px ${backgroundColor}`
+            : "0px 0px 20px transparent"
+        }
+      >
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+          height={36}
+        >
+          {isIconLeft ? (
+            <>
+              <Icon icon={icon} size="MEDIUM" />
+              <Text type="button" color="white">
+                {children ? children : ""}
+              </Text>
+            </>
+          ) : (
+            <>
+              <Text type="button" color="white">
+                {children ? children : ""}
+              </Text>
+              <Icon icon={icon} size="MEDIUM" />
+            </>
+          )}
+        </Flex>
+      </Box>
+    </div>
+  );
+};
+
+export default IconButton;
