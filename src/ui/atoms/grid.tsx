6d41e780e97d9209a77b7da2dc8c20ecf01e399e
0xsnowman <eth.snowman@gmail.com>
2022-10-05 02:07:09 +0800

    atom-grid-add

---
+++ src/ui/atoms/grid.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface IGridProps {
+  children: React.ReactNode;
+  className?: string;
+  gap?: number;
+  alignCenter?: boolean;
+}
+
+const Grid: React.FC<IGridProps> = ({
+  children,
+  className,
+  gap,
+  alignCenter
+}) => {
+  return (
+    <div
+      className={["atom-grid", className].join(" ")}
+      style={{ gap: gap, alignItems: alignCenter ? "center" : "initial" }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Grid;
