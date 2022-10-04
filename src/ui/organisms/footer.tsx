e0ecd8fc17cb14b1e611129692872ffb8776582c
0xsnowman <eth.snowman@gmail.com>
2022-10-05 02:08:22 +0800

    organism-footer-change

--- src/ui/organisms/footer.tsx
+++ src/ui/organisms/footer.tsx
@@ -1,13 +1,39 @@
 import React from "react";
 
-import { Box } from "ui/atoms";
+import { Box, Container, Flex, Text, Icon } from "ui/atoms";
+import { NavItem } from "ui/molecules";
+
+import Icons from "assets/icons";
 
 import { PADDING } from "config/dimensions";
+import { COLORS } from "config/colors";
 
 const Footer = () => {
   return (
     <Box className="organism-footer" paddingVertical={PADDING.SUPER_LARGE}>
-      <p></p>
+      <Container>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Flex flexDirection="column" gap={40}>
+            <Text color="white" type="title">
+              Proof Audit
+            </Text>
+            <Text color={COLORS.GRAY_DARK} type="plain">@ 2018 by TechRate. All Rights Reserved</Text>
+          </Flex>
+          <Flex gap={40} alignItems="center">
+            <Flex gap={5}>
+              <NavItem>Products</NavItem>
+              <NavItem>Blog</NavItem>
+              <NavItem>About Us</NavItem>
+              <NavItem>Contact</NavItem>
+            </Flex>
+            <Flex gap={30}>
+              <Icon icon={Icons.telegram} size="LARGE" />
+              <Icon icon={Icons.discord} size="LARGE" />
+              <Icon icon={Icons.github} size="LARGE" />
+            </Flex>
+          </Flex>
+        </Flex>
+      </Container>
     </Box>
   );
 };
