ea8175bafc16a9154484cb9ce77ba0787bab0183
0xsnowman <eth.snowman@gmail.com>
2022-10-05 02:06:54 +0800

    asset-add

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,11 +1,21 @@
 import rocket from "./rocket.svg";
 import blockchain from "./blockchain.svg";
 import search from "./search.svg";
+import left from "./left.svg";
+import right from "./right.svg";
+import discord from "./discord.svg";
+import telegram from "./telegram.svg";
+import github from "./github.svg";
 
 const Icons = {
     rocket,
     blockchain,
     search,
+    left,
+    right,
+    github,
+    telegram,
+    discord
 };
 
 export default Icons;
