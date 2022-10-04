6daa7763691c9241269cb773650ee34b36d2a98b
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:12:56 +0800

    images-add

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import slide1 from "./slide1.png";
+import slide2 from "./slide2.png";
+
+const Images = {
+  slide1,
+  slide2
+};
+
+export default Images;
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import Animations from "./animations";
+import Icons from "./icons";
+import Images from './images';
+
+export { Animations, Icons, Images };
