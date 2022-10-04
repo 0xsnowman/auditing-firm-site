70e13865304e3044290d1428be7e9e563dff5b88
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:13:11 +0800

    app-tsx-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Header, Footer } from "./ui/organisms";
-import Home from "./ui/pages/home";
+import { Header, Footer } from "ui/organisms";
+import Home from "ui/pages/home";
 
-import "./styles/index.scss";
+import "styles/index.scss";
 
 function App() {
   return (
