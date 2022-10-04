ed5532ef9e4bc2f5fd4cb977025ea4b84b00a6e3
0xsnowman <eth.snowman@gmail.com>
2022-10-04 17:14:26 +0800

    atom-image-add

---
+++ src/ui/atoms/image.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface IImageProps {
+  image: string;
+  className?: string;
+  width: string;
+  height: string;
+  objectFit?: "contain" | "cover" | "fill" | "revert" | "scaleDown";
+}
+
+const Image: React.FC<IImageProps> = ({
+  image,
+  className,
+  width,
+  height,
+  objectFit = "contain"
+}) => {
+  const objectFitClassNames = {
+    contain: "atom-image-contain",
+    cover: "atom-image-cover",
+    fill: "atom-image-fill",
+    revert: "atom-image-revert",
+    scaleDown: "atom-image-scaleDown"
+  };
+  return (
+    <img
+      className={["atom-image", className, objectFitClassNames[objectFit]].join(
+        " "
+      )}
+      src={image}
+      alt="image"
+      width={width}
+      height={height}
+    />
+  );
+};
+
+export default Image;
