634d669d88785f5ca7e4cfd2837a9f2a24a44b3c
0xsnowman <eth.snowman@gmail.com>
2022-10-06 19:49:41 +0800

    molecule-tip-fix

--- src/ui/molecules/tip.tsx
+++ src/ui/molecules/tip.tsx
@@ -6,12 +6,13 @@ import { COLORS } from "config/colors";
 
 interface ITipProps {
   children?: React.ReactNode;
+  color?: string;
 }
 
-const Tip: React.FC<ITipProps> = ({ children }) => {
+const Tip: React.FC<ITipProps> = ({ children, color = COLORS.WHITE }) => {
   return (
     <Box backgroundColor={COLORS.TIP} paddingVertical={5} paddingHorizontal={10} borderRadius={10}>
-      <Text color={COLORS.TIP_DARK} type="plain">{children}</Text>
+      <Text color={color} type="plain">{children}</Text>
     </Box>
   );
 };
