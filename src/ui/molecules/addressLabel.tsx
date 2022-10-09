473c693fb6746764312da12ed270c4f33e9b5986
0xsnowman <eth.snowman@gmail.com>
2022-10-09 18:00:46 +0800

    molecule-addresslabel-add

---
+++ src/ui/molecules/addressLabel.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// import { Text } from "ui/atoms";
+
+interface IAddressLabelProps {
+    shortened: boolean;
+    address: string;
+};
+
+const AddressLabel:React.FC<IAddressLabelProps> = ({ shortened, address }) => {
+    if (shortened) {
+        return <>{address.substr(0, 5) + "..." + address.substr(address.length - 4, 4)}</>
+    }
+
+    return (
+        <>{address}</>
+    );
+};
+
+export default AddressLabel;
