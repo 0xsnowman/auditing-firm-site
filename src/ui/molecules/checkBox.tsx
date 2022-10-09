2c15583f66e2733cd86dc5e4a53499bf4e8d9cc5
0xsnowman <eth.snowman@gmail.com>
2022-10-09 18:01:18 +0800

    molecule-checkbox-add

---
+++ src/ui/molecules/checkBox.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+import { Icon } from "ui/atoms";
+
+import Icons from "assets/icons";
+
+interface ICheckBoxProps {
+  unCheckedIcon?: string;
+  checkedIcon?: string;
+  defaultCheckState?: boolean;
+  stateChangable?: boolean;
+}
+
+const CheckBox: React.FC<ICheckBoxProps> = ({
+  unCheckedIcon = Icons.star,
+  checkedIcon = Icons.starBlack,
+  defaultCheckState = false,
+  stateChangable = true
+}) => {
+  const [checked, setChecked] = useState<boolean>(defaultCheckState);
+  useEffect(() => {
+      setChecked(defaultCheckState);
+  }, [defaultCheckState]);
+  return (
+    <Icon
+      icon={checked ? checkedIcon : unCheckedIcon}
+      onClick={() => {
+        if (stateChangable) {
+          setChecked(!checked);
+        }
+      }}
+    />
+  );
+};
+
+export default CheckBox;
