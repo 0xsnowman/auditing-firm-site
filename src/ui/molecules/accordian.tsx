1504ce38439ff14d4e7df8a338decb7c65dc69dd
0xsnowman <eth.snowman@gmail.com>
2022-10-10 14:15:27 +0800

    molecule-accordian-add

---
+++ src/ui/molecules/accordian.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+import { Box, Flex, Text, Icon } from "ui/atoms";
+
+import { COLORS } from "config/colors";
+
+import Icons from "assets/icons";
+
+interface IAccordianContentTypes {
+  title: string;
+  content: string;
+  collapse: boolean;
+}
+
+interface IAccordianProps {
+  contents: Array<IAccordianContentTypes>;
+}
+
+const Accordian: React.FC<IAccordianProps> = ({ contents }) => {
+  const [content, setContent] = useState<Array<IAccordianContentTypes>>(
+    contents
+  );
+  return (
+    <Box width="100%" backgroundColor="#999">
+      <Flex flexDirection="column" gap={5}>
+        {content.length === 0 && (
+          <Box backgroundColor={COLORS.GREY} padding={50}>
+            <Text type="subtitle">"No FAQs"</Text>
+          </Box>
+        )}
+        {content.length > 0 &&
+          content.map((contentItem, index) => {
+            return (
+              <Box
+                backgroundColor={COLORS.GREY}
+                padding={20}
+                cursor
+                onClick={() => {
+                  const newContent = content.map((contentItem, idx) => {
+                    if (idx === index)
+                      contentItem.collapse = !contentItem.collapse;
+                    else contentItem.collapse = true;
+                    return contentItem;
+                  });
+                  setContent(newContent);
+                }}
+              >
+                <Box transition={1}>
+                  <Flex gap={20} flexDirection="column" justifyContent="center">
+                    <Flex alignItems="center" gap={15}>
+                      <Text type="logo" color={COLORS.LIGHT_BLUE}>
+                        {!contentItem.collapse ? (
+                          <Icon icon={Icons.minus} size="MEDIUM"></Icon>
+                        ) : (
+                          <Icon icon={Icons.plus} size="MEDIUM"></Icon>
+                        )}
+                      </Text>
+                      <Text type="paragraph" fontWeight={500}>{contentItem.title}</Text>
+                    </Flex>
+                    {!contentItem.collapse && (
+                      <Text type="paragraph">{contentItem.content}</Text>
+                    )}
+                  </Flex>
+                </Box>
+              </Box>
+            );
+          })}
+      </Flex>
+    </Box>
+  );
+};
+
+export default Accordian;
