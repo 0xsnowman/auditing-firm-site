cbb469e1a4488bb56b841d14a08c916c18b64b36
0xsnowman <eth.snowman@gmail.com>
2022-10-11 23:28:09 +0800

    molecule-milestone-fix

--- src/ui/molecules/milestone.tsx
+++ src/ui/molecules/milestone.tsx
@@ -1,52 +1,64 @@
 import React from "react";
-
 import { Box, Flex, Text } from "ui/atoms";
 import { PostBox } from "ui/molecules";
-
 import { COLORS } from "config/colors";
 import { Z_INDEX_LEVELS } from "config/dimensions";
 
-const MileStone = () => {
+interface IMileStoneProps {
+  value: number;
+  direction?: "horizontal" | "vertical";
+}
+
+const MileStone: React.FC<IMileStoneProps> = ({
+  value,
+  direction = "horizontal"
+}) => {
   return (
     <Box position="relative">
-      <Box
-        backgroundColor={COLORS.GRAY_DARK}
-        padding={1}
-        position="absolute"
-        top={14}
-        left={0}
-        width="100%"
-      ></Box>
-      <Flex justifyContent="center">
+      <Flex flexDirection={direction === "horizontal" ? "column" : "row"}>
         <Box
-          width={30}
-          height={30}
-          backgroundColor={COLORS.LIGHT_BLUE}
-          zIndex={Z_INDEX_LEVELS.SUPER}
-          borderRadius={30}
-          padding={0}
-        >
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="100%"
-            height="100%"
+          backgroundColor={COLORS.GRAY_DARK}
+          padding={1}
+          position="absolute"
+          top={direction === "horizontal" ? 14 : 0}
+          left={direction === "horizontal" ? 0 : 14}
+          width={direction === "horizontal" ? "100%" : "auto"}
+          height={direction === "horizontal" ? "auto" : "100%"}
+        ></Box>
+        <Flex justifyContent="center" alignItems="center">
+          <Box
+            width={30}
+            height={30}
+            backgroundColor={COLORS.LIGHT_BLUE}
+            zIndex={Z_INDEX_LEVELS.SUPER}
+            borderRadius={30}
+            padding={0}
           >
-            <Text color="white">1</Text>
-          </Flex>
-        </Box>
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              width="100%"
+              height="100%"
+            >
+              <Text color="white">{value}</Text>
+            </Flex>
+          </Box>
+        </Flex>
+        <PostBox
+          subtitle="Time-Efficient Process"
+          content={[
+            "The length of the audit process is decided during the negotiation stage. As soon as the client has provided all the required documents the audit process can begin."
+          ]}
+          border={false}
+          background="white"
+          image={false}
+          subtitleColor={COLORS.LIGHT_BLUE}
+          borderRadius={0}
+          subtitleCenter={true}
+          paddingVertical={0}
+          leftTextCount={direction === "horizontal" ? 100 : 200}
+        />
       </Flex>
-      <PostBox
-        subtitle="Time-Efficient Process"
-        content={["The length of the audit process is decided during the negotiation stage. As soon as the client has provided all the required documents the audit process can begin."]}
-        border={false}
-        background="white"
-        image={false}
-        subtitleColor={COLORS.LIGHT_BLUE}
-        borderRadius={0}
-        subtitleCenter={true}
-        paddingVertical={0}
-      />
     </Box>
   );
 };
