0ec4401ccfc2a33f69d56733ad36055f9178c059
0xsnowman <eth.snowman@gmail.com>
2022-10-17 18:07:34 +0800

    molecule-searchInput-fix

--- src/ui/molecules/searchInput.tsx
+++ src/ui/molecules/searchInput.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Box, Input, Icon } from "ui/atoms";
 import Icons from "assets/icons";
 import { Z_INDEX_LEVELS } from "config/dimensions";
+import { COLORS } from "config/colors";
+import useWindowDimensions from "hooks/useWindowDimensions";
+import { WINDOW_SIZES } from "config/dimensions";
 
 interface ISearchInputProps {
   placeholder?: string;
@@ -18,6 +21,7 @@ const SearchInput: React.FC<ISearchInputProps> = ({
 }) => {
   const [searchText, setSearchText] = useState<string>("");
   const [extendState, setExtendState] = useState<boolean>(extend);
+  const { deviceWidth } = useWindowDimensions();
   const onClickSearchButton = () => {
     alert(["[Search] [Input] -", searchText].join(" "));
   };
@@ -25,6 +29,11 @@ const SearchInput: React.FC<ISearchInputProps> = ({
     <Box
       position="relative"
       zIndex={Z_INDEX_LEVELS.MAXIMUM}
+      className="molecule-searchinput"
+      borderColor={COLORS.DARK_THEME_GREY_BACKGROUND}
+      borderWidth={1}
+      borderRadius={10}
+      width={deviceWidth > WINDOW_SIZES.SIZE_464 ? 360 : "100%"}
       onMouseEnter={() => {
         if (extendChangable) {
           setExtendState(true);
@@ -35,7 +44,6 @@ const SearchInput: React.FC<ISearchInputProps> = ({
           setExtendState(false);
         }
       }}
-      className="molecule-searchinput"
     >
       <Box
         position="absolute"
@@ -49,6 +57,7 @@ const SearchInput: React.FC<ISearchInputProps> = ({
       </Box>
       <Input
         width={extendState ? "100%" : "0px"}
+        padding={"15px 40px 15px 70px"}
         className={size}
         value={searchText}
         onChange={evt => {
