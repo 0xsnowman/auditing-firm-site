c8264f2f3bc3e7ef00e3f0758edafe26cc55c257
0xsnowman <eth.snowman@gmail.com>
2022-10-17 18:05:43 +0800

    molecule-mileStone-fix

--- src/ui/molecules/milestone.tsx
+++ src/ui/molecules/milestone.tsx
@@ -7,24 +7,35 @@ import { Z_INDEX_LEVELS } from "config/dimensions";
 interface IMileStoneProps {
   value: number;
   direction?: "horizontal" | "vertical";
+  title?: string;
+  subtitle?: string;
+  content?: Array<string>;
 }
 
 const MileStone: React.FC<IMileStoneProps> = ({
   value,
+  title = "1 DAY",
+  subtitle = "Time-Efficient Process",
+  content = [
+    "The length of the audit process is decided during the negotiation stage. As soon as the client has provided all the required documents the audit process can begin."
+  ],
   direction = "horizontal"
 }) => {
   return (
     <Box position="relative">
-      <Flex flexDirection={direction === "horizontal" ? "column" : "row"}>
+      <Flex
+        flexDirection={direction === "horizontal" ? "column" : "row"}
+        gap={20}
+      >
         <Box
-          backgroundColor={COLORS.GREY}
-          padding={1}
+          backgroundColor={COLORS.DARK_THEME_GREY_BACKGROUND}
+          padding={3}
           position="absolute"
           top={direction === "horizontal" ? 14 : 0}
           left={direction === "horizontal" ? 0 : 14}
           width={direction === "horizontal" ? "100%" : "auto"}
           height={direction === "horizontal" ? "auto" : "100%"}
-        ></Box>
+        />
         <Flex justifyContent="center" alignItems="center">
           <Box
             width={30}
@@ -40,25 +51,27 @@ const MileStone: React.FC<IMileStoneProps> = ({
               width="100%"
               height="100%"
             >
-              <Text color="white">{value}</Text>
+              <Text color={COLORS.WHITE}>{value}</Text>
             </Flex>
           </Box>
         </Flex>
         <PostBox
-          subtitle="Time-Efficient Process"
-          content={[
-            "The length of the audit process is decided during the negotiation stage. As soon as the client has provided all the required documents the audit process can begin."
-          ]}
+          title={title}
+          subtitle={subtitle}
+          content={content}
           border={false}
-          background="white"
+          background="black"
+          titleColor={COLORS.DARK_THEME_WHITE}
           image={false}
-          subtitleColor={COLORS.LIGHT_BLUE}
+          subtitleColor={COLORS.DARK_THEME_WHITE}
           borderRadius={0}
+          textColor={COLORS.DARK_THEME_WHITE}
           subtitleCenter={true}
           paddingVertical={0}
-          leftTextCount={direction === "horizontal" ? 100 : 200}
+          leftTextCount={direction === "horizontal" ? 100 : "all"}
         />
       </Flex>
+      <Box padding={10} />
     </Box>
   );
 };
