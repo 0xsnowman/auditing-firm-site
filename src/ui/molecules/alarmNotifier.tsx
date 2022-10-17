857f81c337441e10a0e7db7a9e3e7dfd72f53ece
0xsnowman <eth.snowman@gmail.com>
2022-10-17 18:03:59 +0800

    molecule-alarm-notifier-fix

--- src/ui/molecules/alarmNotifier.tsx
+++ src/ui/molecules/alarmNotifier.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Box } from "ui/atoms";
 
+import { COLORS } from "config/colors";
+
 const AlarmNotifier = () => {
   return (
     <Box position="relative">
       <Box
         borderRadius={30}
-        width={6}
-        height={6}
-        backgroundColor="red"
-        boxShadow="5px 5px 20px red"
+        width={10}
+        height={10}
+        backgroundColor={COLORS.ALARM}
+        boxShadow="5px 5px 20px white"
       />
     </Box>
   );
