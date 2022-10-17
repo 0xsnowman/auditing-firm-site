ffe0b70941b300bde781e19d32d585b7c49a30b3
0xsnowman <eth.snowman@gmail.com>
2022-10-17 21:30:26 +0800

    misellaneous-changes-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Header, Footer } from "ui/organisms";
-import { Home, Audit, Smart } from "ui/pages";
+import {
+  Audit,
+  Smart,
+  DApp,
+  NFT,
+  Ethereum,
+  BNB,
+  Avalanche,
+  Polygon,
+  Tron
+} from "ui/pages";
 import "styles/index.scss";
 
 function App() {
@@ -9,10 +19,15 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/audit" element={<Audit />} />
-        <Route path="/smart" element={<Smart />} />
+        <Route path="/" element={<Audit />} />
+        <Route path="/security-audit" element={<Smart />} />
+        <Route path="/dapp-audit" element={<DApp />} />
+        <Route path="/nft-audit" element={<NFT />} />
+        <Route path="/ethereum" element={<Ethereum />} />
+        <Route path="/bnb" element={<BNB />} />
+        <Route path="/avalanche" element={<Avalanche />} />
+        <Route path="/polygon" element={<Polygon />} />
+        <Route path="/tron" element={<Tron />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/ui/pages/index.ts
+++ src/ui/pages/index.ts
@@ -1,9 +1,21 @@
 import Audit from "./audit";
-import Home from "./home";
 import Smart from "./smart";
+import DApp from "./dapp";
+import NFT from "./nft";
+import Ethereum from "./ethereum";
+import BNB from "./bnb";
+import Avalanche from "./avalanche";
+import Polygon from "./polygon";
+import Tron from "./tron";
 
 export {
     Audit,
-    Home,
-    Smart
+    Smart,
+    DApp,
+    NFT,
+    Ethereum,
+    BNB,
+    Avalanche,
+    Polygon,
+    Tron
 };
