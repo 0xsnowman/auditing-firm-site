ca623ab48c1002a1f141737ec9fd265d878aed2d
0xsnowman <eth.snowman@gmail.com>
2022-10-17 18:03:08 +0800

    atom-bell-add

---
+++ src/ui/molecules/bell.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Icon, Box } from "ui/atoms";
+import { AlarmNotifier } from "ui/molecules";
+
+import Icons from "assets/icons";
+
+const Bell = () => {
+  return (
+    <Box position="relative">
+      <Icon icon={Icons.alarm} size="MEDIUM" />
+      <Box position="absolute" top={-2} right={-2}>
+        <AlarmNotifier />
+      </Box>
+    </Box>
+  );
+};
+
+export default Bell;
