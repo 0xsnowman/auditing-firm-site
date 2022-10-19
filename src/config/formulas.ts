bfc99705f2494cb264799eaa7c1bf26c91c06011
0xsnowman <eth.snowman@gmail.com>
2022-10-19 21:08:04 +0800

    config-formulas-fix

--- src/config/formulas.ts
+++ src/config/formulas.ts
@@ -1,14 +1,11 @@
 export const fixedScrollBarLeftCalculate = (deviceWidth: number) => {
-  if (deviceWidth > 1800) {
-    return `calc(${(deviceWidth - 1350) / 2}px + 88px)`;
+  if (deviceWidth > 2400) {
+    return `calc(${(deviceWidth - 1800) / 2}px + 41px)`;
+  } else if (deviceWidth > 1800) {
+    return `calc(${(deviceWidth - 1450) / 2}px + 41px)`;
   } else if (deviceWidth > 1200) {
     return `calc(${(deviceWidth - 1150) / 2}px + 41px)`;
   } else if (deviceWidth > 900) {
     return `calc(${(deviceWidth - 860) / 2}px + 41px)`;
-  } else if (deviceWidth > 464) {
-    return 43 + 20;
-  } else if (deviceWidth > 342) {
-    return 43 + 20;
-  }
-  return 41;
+  } return 43 + 20;
 };
